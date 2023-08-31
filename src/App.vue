618bff50fcbfba07756aca81376d625f9cbb6848
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-09-01 00:08:14 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script>
-
+// importiamo
+import AppMain from './components/AppMain.vue'
+export default{
+  components:{
+    AppMain
+  }
+}
 </script>
 
 <template>
-  
+  <div>
+    <AppMain></AppMain>
+  </div>
 </template>
 
 <style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            
+        }
+    },
+    created() {
+        
+    },
+  
+}
+</script>
+
+<template lang="">
+  <div>
+    ciao a tutti
+  </div>
+</template>
+
+<style lang="">
+  
+</style>
